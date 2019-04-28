02f8bb14a65fbd31b8ff23e6f00d11addb650917
longquanxiao <1186481819@qq.com>
2019-04-28 22:32:48 +0800

    hh
    gg

---
+++ src/com/Config/Config.java
@@ -0,0 +1,13 @@
+package com.Config;
+
+/*
+ * 服务器配置类
+ */
+public class Config {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("测试成功");
+	}
+
+}
---
+++ src/comframe/Mainframe.java
@@ -0,0 +1,8 @@
+package comframe;
+/*
+ 这是主类的页面
+ */
+public class Mainframe {
+
+	
+}
